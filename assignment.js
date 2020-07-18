e8d6781d3eb94a26dc955cf1e9eed4849052fecd
Masum984 <66863097+Masum984@users.noreply.github.com>
2020-07-18 16:37:55 +0600

    mybe it's finished

--- assignment.js
+++ assignment.js
@@ -1,23 +1,43 @@
       // feetToMile
     function feetToMile(feet){
-    var mile = feet / 5280;
-
-     return mile;
-    }
-   var result = feetToMile(132000);
-   console.log(result);
+        var mile = feet / 5280;
+        return mile;
+        }
+        var result = feetToMile(132000);
+        console.log(result);
 
 
 
     //   woodCalculator
-function woodCalculator(chair, table, bed){
-    var chairCount = chair * 1;
-    var tableCount = table * 2;
-    var bedCount = bed * 5;
-    var totalwood = chair + table + bed;
-    return totalwood;
-}
-var woodresult = woodCalculator(14, 5, 12);
-console.log(woodresult);
+  function woodCalculator(chair, table, bed){
+        var chairCount = chair * 1;
+        var tableCount = table * 3;
+        var bedCount   = bed   * 5;
+        var totalwood  = chair + table + bed;
+
+        return totalwood;
+    }
+        var woodresult = woodCalculator(14, 5, 12);
+        console.log(woodresult);
 
 
+
+        //    tinyFriend
+   function tinyFriend(friends){
+     
+         var tiny = friends[0];
+     for(var i = 0; i < friends.lenght; i++){
+        var currentfriend = friends[i];
+        if(currentfriend.lenght <= tiny.lenght){
+          tiny = currentfriend;
+       }
+
+     }
+     return tiny;
+   }
+  var tinyboy = tinyFriend([ 'MasumBillah','sumon', 'Umayma', 'kaku']);
+  console.log(tinyboy);
+
+
+
+  
